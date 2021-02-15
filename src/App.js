31c68fd0d37a56563d97cf19b523f06eccbecaf3
Fabio Lisboa <52582229+lisboadivalu@users.noreply.github.com>
2021-02-15 04:48:53 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
+import React, { useState } from "react";
 import "./styles.css";
+
 import Navbar from "./components/navbar/navbar";
+import TaskList from "./components/tasklist/tasklist";
+
+let idAcc = 0;
+const generateId = () => {
+  idAcc = idAcc + 1;
+  return idAcc;
+};
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+  const addTask = (title, state) => {
+    const newTask = {
+      id: generateId(),
+      title,
+      state
+    };
+    setTasks((existingTasks) => {
+      return [...existingTasks, newTask];
+    });
+  };
+
   return (
     <div className="App">
       <Navbar />
-      <ul>
-        <li>Aprenderemos React</li>
-        <li>Aprenderemos Component</li>
-      </ul>
+      <div className="container">
+        <TaskList title="Pendente" onAddTask={addTask} tasks={tasks} />
+      </div>
     </div>
   );
 }
